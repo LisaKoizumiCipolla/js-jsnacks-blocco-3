6bd1c0d4607b471ba9e52706d0e5879fbb63d094
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-06-15 13:08:38 +0200

    Snack

---
+++ W3Header/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import NewHeader from './components/NewHeader.vue'
+
+export default {
+    name: 'App',
+    components:{
+      NewHeader
+    }
+}
+</script>
+
+<template>
+  <div>
+    <NewHeader />
+  </div>
+  
+</template>
+
+<style lang="scss">
+@import "./styles/general.scss";
+</style>
---
+++ W3Header/src/components/NewHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <ul>
+        <li v-for="link in links">
+            {{ link }}
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default{
+        data() {
+            return{
+            links : [
+                "uno",
+                "due",
+                "tre",
+                "quattro"
+            ]
+
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+
+ul{
+    list-style-type: none;
+    display: flex;
+
+    li{
+        margin-left: 2rem;
+        color: black;
+        font-size: 5rem;
+
+        &:hover{
+            color: magenta;
+            cursor: pointer;
+        }
+    }
+}
+
+</style>
